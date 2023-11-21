62da4af1b2376d67e0377d62f0c0cd92be6a6825
Piyush Patel <136020845+piyushpatelcodes@users.noreply.github.com>
2023-11-21 23:51:05 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import '@/styles/globals.css';
-import Navigation from './Navbar.jsx'; // Assuming you have a Navigation component
-import WriteBlogPage from './WriteBlogPage'; // Assuming you have a WriteBlogPage component
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <title>NewsX</title>
-      <Navigation />
-      <Component {...pageProps} />
-    </>
-  );
+  <>
+  <title>
+    NewsX
+  </title>
+  <Component {...pageProps} />
+  </>
+  )
 }
